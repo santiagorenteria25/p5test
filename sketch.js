919afceea086e725321c594da15d0ee544700266
santiagorenteriastudent <39573144+santiagorenteriastudent@users.noreply.github.com>
2021-08-03 17:58:23 +0800

    Update

--- sketch.js
+++ sketch.js
@@ -1,49 +1,69 @@
 /*
- * @name Playback Rate
- * @description <p>Load a SoundFile and map its playback rate to
- * mouseY, volume to mouseX. Playback rate is the speed with
- * which the web audio context processings the sound file information.
- * Slower rates not only increase the duration of the sound, but also
- * decrease the pitch because it is being played back at a slower frequency.</p>
+ * @name Sound Effect
+ * @description <p>Play a sound effect when the mouse is clicked inside the circle.</p>
  * <p><em><span class="small"> To run this example locally, you will need the
  * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
  * a sound file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.</span></em></p>
  */
-// A sound file object
-let song;
+// Adapted from Learning Processing by Daniel Shiffman
+// http://www.learningprocessing.com
+// Doorbell sample by Corsica_S via freesound.org,
+// Creative Commons BY 3.0
 
-function preload() {
-  // Load a sound file
-  song = loadSound('assets/Damscray_DancingTiger.mp3');
+// A Class to describe a "doorbell" (really a button)
+class Doorbell {
+  constructor(x_, y_, r_) {
+    // Location and size
+    this.x = x_;
+    this.y = y_;
+    this.r = r_;
+  }
+  // Is a point inside the doorbell? (used for mouse rollover, etc.)
+  contains(mx, my) {
+    return dist(mx, my, this.x, this.y) < this.r;
+  }
+
+  // Show the doorbell (hardcoded colors, could be improved)
+  display(mx, my) {
+    if (this.contains(mx, my)) {
+      fill(100);
+    } else {
+      fill(175);
+    }
+    stroke(0);
+    strokeWeight(4);
+    ellipseMode(RADIUS);
+    ellipse(this.x, this.y, this.r, this.r);
+  }
 }
 
-function setup() {
-  createCanvas(710, 400);
+// A sound file object
+let dingdong;
 
-  // Loop the sound forever
-  // (well, at least until stop() is called)
-  song.loop();
+// A doorbell object (that will trigger the sound)
+let doorbell;
+
+function setup() {
+  createCanvas(200, 200);
+
+  // Load the sound file.
+  // We have included both an MP3 and an OGG version.
+  soundFormats('mp3', 'ogg');
+  dingdong = loadSound('assets/doorbell.mp3');
+
+  // Create a new doorbell
+  doorbell = new Doorbell(width / 2, height / 2, 32);
 }
 
 function draw() {
-  background(200);
+  background(255);
+  // Show the doorbell
+  doorbell.display(mouseX, mouseY);
+}
 
-  // Set the volume to a range between 0 and 1.0
-  let volume = map(mouseX, 0, width, 0, 1);
-  volume = constrain(volume, 0, 1);
-  song.amp(volume);
-
-  // Set the rate to a range between 0.1 and 4
-  // Changing the rate alters the pitch
-  let speed = map(mouseY, 0.1, height, 0, 2);
-  speed = constrain(speed, 0.01, 4);
-  song.rate(speed);
-
-  // Draw some circles to show what is going on
-  stroke(0);
-  fill(51, 100);
-  ellipse(mouseX, 100, 48, 48);
-  stroke(0);
-  fill(51, 100);
-  ellipse(100, mouseY, 48, 48);
+function mousePressed() {
+  // If the user clicks on the doorbell, play the sound!
+  if (doorbell.contains(mouseX, mouseY)) {
+    dingdong.play();
+  }
 }
